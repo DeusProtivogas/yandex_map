b9e1d2a336edb946b56cd1447e3c603aafc7107f
DeusProtivogas <kirill2689@gmail.com>
2024-04-03 20:23:31 +0300

    Moved views to places app

--- places/views.py
+++ places/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import HttpResponse, JsonResponse
+from django.template import loader
+from django.shortcuts import get_object_or_404
+from django.urls import reverse
+
+from places.models import Place
+
+
+def get_locations():
+    locations = Place.objects.all()
+    locations_json = {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [place.coordinates_lon, place.coordinates_lat]
+                },
+                "properties": {
+                    "title": place.title,
+                    "placeId": place.placeId,
+                    "detailsUrl": reverse(place_detail_view , args=[place.placeId]),
+                }
+            } for place in locations
+        ]
+    }
+    return locations_json
+
+def place_detail_view(request, place_id):
+    place = Place.objects.filter(placeId=place_id).first()
+
+    place_json = {
+        "title": place.title,
+        "imgs": [
+           img.upload.url for img in list(place.photos.all().order_by('position'))
+        ],
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {
+            "lng": place.coordinates_lon,
+            "lat": place.coordinates_lat,
+        }
+    }
+    return JsonResponse(place_json, safe=False, json_dumps_params={'ensure_ascii': False, 'indent': 4})
+
+
+def main_page(request):
+
+    locations = get_locations()
+
+    template = loader.get_template('index.html')
+    context = {"locations": locations}
+    rendered_page = template.render(context, request)
+    return HttpResponse(rendered_page)
+
--- yandex_map/urls.py
+++ yandex_map/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 # from yandex_map import settings
 from django.conf import settings
 
-from yandex_map.views import main_page, place_detail_view
+from places.views import main_page, place_detail_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
