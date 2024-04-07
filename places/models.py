ff755d18353668df2187f4006fc6de606501e583
DeusProtivogas <kirill2689@gmail.com>
2024-04-07 19:04:07 +0300

    Added verbose names

--- places/models.py
+++ places/models.py
@@ -3,13 +3,30 @@ from tinymce.models import HTMLField
 
 
 class Place(models.Model):
-    placeId = models.BigAutoField(primary_key=True)
-    title = models.CharField(max_length=200, unique=True)
-    short_description = models.TextField(blank=True)
-    long_description = HTMLField(blank=True)
+    placeId = models.BigAutoField(
+        primary_key=True,
+        verbose_name="Уникальный ключ заведения",
+    )
+    title = models.CharField(
+        max_length=200,
+        unique=True,
+        verbose_name="Название заведения",
+    )
+    short_description = models.TextField(
+        blank=True,
+        verbose_name="Краткое описание",
+    )
+    long_description = HTMLField(
+        blank=True,
+        verbose_name="Полное описание",
+    )
 
-    coordinates_lon = models.FloatField()
-    coordinates_lat = models.FloatField()
+    coordinates_lon = models.FloatField(
+        verbose_name="Долгота",
+    )
+    coordinates_lat = models.FloatField(
+        verbose_name="Широта",
+    )
 
     class Meta:
         ordering = ["placeId"]
@@ -21,11 +38,18 @@ class Place(models.Model):
 
 
 class Photo(models.Model):
-    image = models.ImageField(upload_to='')
-    position = models.IntegerField(default=1, db_index=True)
+    image = models.ImageField(
+        upload_to='',
+        verbose_name="Фотография",
+    )
+    position = models.IntegerField(
+        default=1,
+        db_index=True,
+        verbose_name="Позиция",
+    )
     place = models.ForeignKey(
         Place,
-        verbose_name="Место",
+        verbose_name="Заведение",
         related_name="photos",
         on_delete=models.CASCADE,
     )
